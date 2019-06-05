fac9c9d64ee14616afcc170faea637ec3482ab55
Mark Buskbjerg <tyf5vi@Rsyd.net>
2019-06-05 05:33:33 +0200

    Revert "added a gulp task 'spec' to run jasmine tests using CLI"
    This reverts commit 3df5f8fe02d6a08a89fea3e34253fd395980b1cd.

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,9 @@ var settings = {
   styles: false,
   svgs: false,
   copy: true,
-  reload: true,
+  reload: true
 };
+
 
 /**
  * Paths to project folders
@@ -23,22 +24,23 @@ var paths = {
   scripts: {
     input: 'src/js/*',
     polyfills: '.polyfill.js',
-    output: 'dist/js/',
+    output: 'dist/js/'
   },
   styles: {
     input: 'src/sass/**/*.{scss,sass}',
-    output: 'dist/',
+    output: 'dist/'
   },
   svgs: {
     input: 'src/svg/*.svg',
-    output: 'dist/svg/',
+    output: 'dist/svg/'
   },
   copy: {
     input: 'src/copy/**/*',
-    output: './',
+    output: './'
   },
-  reload: './',
+  reload: './'
 };
+
 
 /**
  * Template for banner to add to file headers
@@ -49,22 +51,19 @@ var banner = {
     '/*!\n' +
     ' * <%= package.name %> v<%= package.version %>\n' +
     ' * <%= package.description %>\n' +
-    ' * (c) ' +
-    new Date().getFullYear() +
-    ' <%= package.author.name %>\n' +
+    ' * (c) ' + new Date().getFullYear() + ' <%= package.author.name %>\n' +
     ' * <%= package.license %> License\n' +
     ' * <%= package.repository.url %>\n' +
     ' */\n\n',
   min:
     '/*!' +
     ' <%= package.name %> v<%= package.version %>' +
-    ' | (c) ' +
-    new Date().getFullYear() +
-    ' <%= package.author.name %>' +
+    ' | (c) ' + new Date().getFullYear() + ' <%= package.author.name %>' +
     ' | <%= package.license %> License' +
     ' | <%= package.repository.url %>' +
-    ' */\n',
+    ' */\n'
 };
+
 
 /**
  * Gulp Packages
@@ -97,17 +96,6 @@ var svgmin = require('gulp-svgmin');
 // BrowserSync
 var browserSync = require('browser-sync');
 
-// Test
-var jasmine = require('gulp-jasmine');
-
-/**
- *
- * TEST TASK SETUP
- */
-var spec = function(done) {
-  src('spec/**.js').pipe(jasmine());
-  return done();
-};
 
 /**
  * Gulp Tasks
@@ -115,69 +103,58 @@ var spec = function(done) {
 
 // Remove pre-existing content from output folders
 var cleanDist = function(done) {
+
   // Make sure this feature is activated before running
   if (!settings.clean) return done();
 
   // Clean the dist folder
-  del.sync([paths.output]);
+  del.sync([
+    paths.output
+  ]);
 
   // Signal completion
   return done();
+
 };
 
 // Repeated JavaScript tasks
 var jsTasks = lazypipe()
-  .pipe(
-    header,
-    banner.full,
-    { package: package }
-  )
+  .pipe(header, banner.full, { package: package })
   .pipe(optimizejs)
-  .pipe(
-    dest,
-    paths.scripts.output
-  )
-  .pipe(
-    rename,
-    { suffix: '.min' }
-  )
+  .pipe(dest, paths.scripts.output)
+  .pipe(rename, { suffix: '.min' })
   .pipe(uglify)
   .pipe(optimizejs)
-  .pipe(
-    header,
-    banner.min,
-    { package: package }
-  )
-  .pipe(
-    dest,
-    paths.scripts.output
-  );
+  .pipe(header, banner.min, { package: package })
+  .pipe(dest, paths.scripts.output);
 
 // Lint, minify, and concatenate scripts
 var buildScripts = function(done) {
+
   // Make sure this feature is activated before running
   if (!settings.scripts) return done();
 
   // Run tasks on script files
-  src(paths.scripts.input).pipe(
-    flatmap(function(stream, file) {
+  src(paths.scripts.input)
+    .pipe(flatmap(function(stream, file) {
+
       // If the file is a directory
       if (file.isDirectory()) {
+
         // Setup a suffix variable
         var suffix = '';
 
         // If separate polyfill files enabled
         if (settings.polyfills) {
+
           // Update the suffix
           suffix = '.polyfills';
 
           // Grab files that aren't polyfills, concatenate them, and process them
-          src([
-            file.path + '/*.js',
-            '!' + file.path + '/*' + paths.scripts.polyfills,
-          ])
+          src([file.path + '/*.js', '!' + file.path + '/*' + paths.scripts.polyfills])
             .pipe(concat(file.relative + '.js'))
             .pipe(jsTasks());
+
         }
 
         // Grab all files and concatenate them
@@ -187,19 +164,22 @@ var buildScripts = function(done) {
           .pipe(jsTasks());
 
         return stream;
+
       }
 
       // Otherwise, process the file
       return stream.pipe(jsTasks());
-    })
-  );
+
+    }));
 
   // Signal completion
   done();
+
 };
 
 // Lint scripts
 var lintScripts = function(done) {
+
   // Make sure this feature is activated before running
   if (!settings.scripts) return done();
 
@@ -210,47 +190,45 @@ var lintScripts = function(done) {
 
   // Signal completion
   done();
+
 };
 
 // Process, lint, and minify Sass files
 var buildStyles = function(done) {
+
   // Make sure this feature is activated before running
   if (!settings.styles) return done();
 
   // Run tasks on all Sass files
   src(paths.styles.input)
-    .pipe(
-      sass({
-        outputStyle: 'expanded',
-        sourceComments: false,
-      })
-    )
-    .pipe(
-      prefix({
-        browsers: ['last 2 version', '> 0.25%'],
-        cascade: true,
-        remove: true,
-      })
-    )
+    .pipe(sass({
+      outputStyle: 'expanded',
+      sourceComments: false
+    }))
+    .pipe(prefix({
+      browsers: ['last 2 version', '> 0.25%'],
+      cascade: true,
+      remove: true
+    }))
     .pipe(header(banner.full, { package: package }))
     .pipe(dest(paths.styles.output))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(
-      minify({
-        discardComments: {
-          removeAll: true,
-        },
-      })
-    )
+    .pipe(minify({
+      discardComments: {
+        removeAll: true
+      }
+    }))
     .pipe(header(banner.min, { package: package }))
     .pipe(dest(paths.styles.output));
 
   // Signal completion
   done();
+
 };
 
 // Optimize SVG files
 var buildSVGs = function(done) {
+
   // Make sure this feature is activated before running
   if (!settings.svgs) return done();
 
@@ -261,34 +239,40 @@ var buildSVGs = function(done) {
 
   // Signal completion
   done();
+
 };
 
 // Copy static files into output folder
 var copyFiles = function(done) {
+
   // Make sure this feature is activated before running
   if (!settings.copy) return done();
 
   // Copy static files
-  src(paths.copy.input).pipe(dest(paths.copy.output));
+  src(paths.copy.input)
+    .pipe(dest(paths.copy.output));
 
   // Signal completion
   done();
+
 };
 
 // Watch for changes to the src directory
 var startServer = function(done) {
+
   // Make sure this feature is activated before running
   if (!settings.reload) return done();
 
   // Initialize BrowserSync
   browserSync.init({
     server: {
-      baseDir: paths.reload,
-    },
+      baseDir: paths.reload
+    }
   });
 
   // Signal completion
   done();
+
 };
 
 // Reload the browser when files change
@@ -304,6 +288,7 @@ var watchSource = function(done) {
   done();
 };
 
+
 /**
  * Export Tasks
  */
@@ -312,13 +297,19 @@ var watchSource = function(done) {
 // gulp
 exports.default = series(
   cleanDist,
-  parallel(buildScripts, lintScripts, buildStyles, buildSVGs, copyFiles)
+  parallel(
+    buildScripts,
+    lintScripts,
+    buildStyles,
+    buildSVGs,
+    copyFiles
+  )
 );
 
 // Watch and reload
 // gulp watch
-exports.watch = series(exports.default, startServer, watchSource);
-
-// Specs Jasmine test
-// gulp spec
-exports.spec = spec;
+exports.watch = series(
+  exports.default,
+  startServer,
+  watchSource
+);
